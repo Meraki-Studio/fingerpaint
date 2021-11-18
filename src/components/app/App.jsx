e90687b2770e5b0fb4c1eba6481115542a540ebb
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-11-18 11:51:30 -0800

    Containers added to App Routing

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-// import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Button from '@mui/material/Button';
 
 export default function App() {
     return (
         <div>
-            {/* build Routes and Route here */}
-            <Button variant="contained">Hello World</Button>
+            <Routes>
+                <Route path="/" element={<Landing />}>
+                    <Route path="login" element={<Login />} />
+                    <Route path="home" element={<Home />} />
+                    <Route path="canvas" element={<Canvas />} />
+                </Route>
+            </Routes>
         </div>
     );
 }
