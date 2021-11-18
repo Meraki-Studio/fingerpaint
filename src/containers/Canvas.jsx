e6336604f981cb0b99b9eef713c60be2df202166
Julianne Vela <75726146+juliannevela@users.noreply.github.com>
2021-11-18 11:06:34 -0800

    initialized remaining pages. Need to add structure + components

---
+++ src/containers/Canvas.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Canvas() {
+    return (
+        <div>
+            <p>This is the canvas page!</p>
+        </div>
+    );
+}
+
+export default Canvas;
---
+++ src/containers/Drawer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Drawer() {
+    return (
+        <div>
+            <p>This is the Drawer!</p>
+        </div>
+    );
+}
+
+export default Drawer;
