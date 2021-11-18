84c48ea9ba87785de2639aa50fe1ad58ee12fb18
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-11-17 20:11:53 -0800

    Provider and First Container - basic setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import {UserProvider} from './state/UserProvider';
 import App from './components/app/App';
 
 render(
-  <Router>
-    <App />
-  </Router>,
+  <UserProvider>
+    <Router>
+      <App />
+    </Router>
+  </UserProvider>,
   document.getElementById('root')
 );
