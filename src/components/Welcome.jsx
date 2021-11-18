7e0f2d9970c4d7d69c074bc4e726f64d794a90bd
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-11-18 12:12:23 -0800

    Welcome message added

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Welcome() {
+    return (
+        <section>
+            <h3>Welcome to Fingerpaint by Meraki</h3>
+        </section>
+    );
+}
