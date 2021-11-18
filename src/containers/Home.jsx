39b7c5fc260d76d1169a7d817c4938c064103679
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-11-18 12:13:18 -0800

    incorporated Artlist vs Welcome rendering, still need header & start button

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useMyArt } from '../state/UserProvider';
+
+import ArtList from '../components/art/ArtList';
+import Welcome from '../components/Welcome';
 
 export default function Home() {
+    const { myArt } = useMyArt();
+    console.log(myArt);
+
     return (
-        <div>
-            <p>This is home!</p>
-        </div>
+        <main>
+            {/* menu + logo */}
+            {myArt ? <ArtList drawings={myArt} /> : <Welcome />}
+        </main>
     );
 }
