a3b6ca70be0bd4ca020c8a386abaffa6d948e86d
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-11-18 16:24:50 -0800

    added spacing

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
 import { useMyArt } from '../state/UserProvider';
+import styles from './containers.css';
 
 import ArtList from '../components/art/ArtList';
 import Welcome from '../components/Welcome';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
 
 export default function Home() {
     const { myArt } = useMyArt();
     console.log(myArt);
 
     return (
-        <main>
+        <main className={styles.home}>
             {/* menu + logo */}
-            {myArt ? <ArtList drawings={myArt} /> : <Welcome />}
+            {myArt ? (
+                <div className={styles.homeArtlist}>
+                    <ArtList drawings={myArt} />
+                    <Button href="canvas" variant="contained">
+                        <AddIcon />
+                    </Button>
+                </div>
+            ) : (
+                <div className={styles.homeNoArtlist}>
+                    <Welcome />
+                    <Button href="canvas" variant="contained" size="large">
+                        <AddIcon /> Create New Art
+                    </Button>
+                </div>
+            )}
         </main>
     );
 }
