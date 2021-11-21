8fd7ecf3723b06a0deed1fcf43e684f6b6d91057
Julianne Vela <75726146+juliannevela@users.noreply.github.com>
2021-11-20 19:02:13 -0800

    header, hero, what, and features areas template laid out

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
+import { CssBaseline } from '@mui/material';
 
 import Landing from '../../containers/Landing';
 import Login from '../../containers/Login';
 import Home from '../../containers/Home';
 import Canvas from '../../containers/Canvas.jsx';
-
-import styles from './App.css';
 
 import {
     faArrowLeft,
@@ -39,13 +38,13 @@ library.add(
 
 export default function App() {
     return (
-        <main>
+        <CssBaseline>
             <Routes>
                 <Route path="/" element={<Landing />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/canvas" element={<Canvas />} />
             </Routes>
-        </main>
+        </CssBaseline>
     );
 }
--- src/containers/Landing.jsx
+++ src/containers/Landing.jsx
@@ -1,10 +1,122 @@
 import React from 'react';
+import { Button, Typography } from '@mui/material';
+
+import styles from './Landing.css';
 
 const Landing = () => {
     return (
-        <section>
-            <p>Welcome to Fingerpaint! This is our landing page!</p>
-        </section>
+        <>
+            <header className={styles.header}>
+                <span className={styles.logo}>Fingerpaint</span>
+                <nav className={styles.navBar}>
+                    <a href="#what">What is it</a>
+                    <a href="#how">How to use it</a>
+                    <a href="#who">Who made it</a>
+                </nav>
+                <Button className={styles.ctaBtn} variant="contained">
+                    Try it for free
+                </Button>
+            </header>
+            <main className={styles.container}>
+                <section className={styles.hero}>
+                    <span className={styles.heroText}>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Ut recusandae modi, repellat beatae non ipsum
+                        ratione expedita autem delectus et officiis. Nesciunt
+                        quae culpa nostrum mollitia voluptate voluptatibus nobis
+                        laudantium.
+                    </span>
+                    <img
+                        className={styles.heroImg}
+                        src="http://place-puppy.com/500x500"
+                        alt="Hero"
+                    />
+                </section>
+                <section className={styles.what}>
+                    <span className={styles.whatText}>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Ut recusandae modi, repellat beatae non ipsum
+                        ratione expedita autem delectus et officiis. Nesciunt
+                        quae culpa nostrum mollitia voluptate voluptatibus nobis
+                        laudantium.
+                    </span>
+                    <img
+                        className={styles.whatImg}
+                        src="http://place-puppy.com/500x500"
+                        alt="Hero"
+                    />
+                </section>
+                <section className={styles.features}>
+                    <article className={styles.featureCard}>
+                        <figure>
+                            <img
+                                src="http://place-puppy.com/50x50"
+                                alt="feature"
+                            />
+                            <figcaption>Interactive Canvas</figcaption>
+                        </figure>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Quisquam, quis.
+                        </p>
+                    </article>
+                    <article className={styles.featureCard}>
+                        <figure>
+                            <img
+                                src="http://place-puppy.com/50x50"
+                                alt="feature"
+                            />
+                            <figcaption>Fun stickers</figcaption>
+                        </figure>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Quisquam, quis.
+                        </p>
+                    </article>
+                    <article className={styles.featureCard}>
+                        <figure>
+                            <img
+                                src="http://place-puppy.com/50x50"
+                                alt="feature"
+                            />
+                            <figcaption>Minimal interface</figcaption>
+                        </figure>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Quisquam, quis.
+                        </p>
+                    </article>
+                </section>
+                {/* <section className={styles.how}>
+                    <span className={styles.howText}>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Ut recusandae modi, repellat beatae non ipsum
+                        ratione expedita autem delectus et officiis. Nesciunt
+                        quae culpa nostrum mollitia voluptate voluptatibus nobis
+                        laudantium.
+                    </span>
+                    <img
+                        className={styles.howImg}
+                        src="http://place-puppy.com/500x500"
+                        alt="Hero"
+                    />
+                </section> */}
+                {/* <section className={styles.what}>
+                    <span className={styles.whatText}>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Ut recusandae modi, repellat beatae non ipsum
+                        ratione expedita autem delectus et officiis. Nesciunt
+                        quae culpa nostrum mollitia voluptate voluptatibus nobis
+                        laudantium.
+                    </span>
+                    <img
+                        className={styles.whatImg}
+                        src="http://place-puppy.com/500x500"
+                        alt="Hero"
+                    />
+                </section> */}
+            </main>
+        </>
     );
 };
 
