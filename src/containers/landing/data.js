1d1781a1f8e74411791a17e75505de0081af3c41
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-11-21 21:57:36 -0800

    Copied over landing page content to merge properly with canvas

---
+++ src/containers/landing/data.js
@@ -0,0 +1,41 @@
+export const makers = [
+  {
+    name: 'Juli Vela',
+    pronoun: 'her',
+    img: './juli.png',
+    role: 'Developer',
+    blurb: 'An engineer with a passion for learning and teaching others, Juli codes to save the planet, advocate for mental health, and encourage other neurodiverse womxn along the way!',
+    links: {
+      portfolio: 'https://',
+      linkedIn: 'https://www.linkedin.com/in/',
+      gitHub: 'https://www.github.com/',
+      twitter: 'https://twitter.com/'
+    }
+  },
+  {
+    name: 'Nick Day',
+    pronoun: 'his',
+    img: './nick.png',
+    role: 'Developer',
+    blurb: 'A creative in pursuit of expression through code, writing, and music, Nick is an engineer with a growth mindset (the power of yet). His experience growing up with an artist and in technical and creative writing gives him an eye for design and clean code.',
+    links: {
+      portfolio: 'https://nickday.info',
+      linkedIn: 'https://www.linkedin.com/in/nickdayfsd',
+      gitHub: 'https://www.github.com/nickdayfsd',
+      twitter: 'https://twitter.com/nickdayfsd'
+    }
+  },
+  {
+    name: 'James',
+    pronoun: 'his',
+    img: './james.png',
+    role: 'Designer',
+    blurb: 'Really just a kid at heart, James loves creating delightful user interfaces and design kits. He always puts the user first. If it’s a joy to use, then it’s a success.',
+    links: {
+      portfolio: 'https://',
+      linkedIn: 'https://www.linkedin.com/in/',
+      gitHub: 'https://www.github.com/',
+      twitter: 'https://twitter.com/'
+    }
+  },
+]
