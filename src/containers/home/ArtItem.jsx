a838d524941705391db345a1d9d7806ae2998609
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-11-23 11:39:31 -0800

    item now draggable

--- src/containers/home/ArtItem.jsx
+++ src/containers/home/ArtItem.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { itemTypes } from '../../utils/itemTypes';
+import { useDrag } from 'react-dnd';
 import styles from '../containers.css';
 
 export default function ArtItem({ art }) {
+    const [{ isDragging }, drag] = useDrag({
+        type: itemTypes.CARD,
+        item: {
+            id: art.id,
+        },
+        collect: (monitor) => ({
+            isDragging: !!monitor.isDragging(),
+        }),
+    });
     return (
-        <label className={styles.artContainer}>
+        <label className={styles.artContainer} ref={drag}>
             <img src={art.img} alt="fingerpainting" className={styles.art} />
         </label>
     );
