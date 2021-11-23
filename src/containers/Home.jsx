51886dfa8d4544035b0289cec172ad1231f51cf7
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-11-22 23:28:11 -0800

    Home page header and buttons added

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -4,34 +4,75 @@ import styles from './containers.css';
 
 import ArtList from '../components/art/ArtList';
 import Welcome from '../components/Welcome';
-import Header from './Header';
 
+import { AppBar, Drawer, Toolbar } from '@mui/material';
 import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
 import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Home() {
-    const { myArt } = useMyArt();
+    const { myArt, setMyArt } = useMyArt();
     console.log(myArt);
+
+    const samples = [
+        'https://placekitten.com/96/140',
+        'https://placekitten.com/96/139',
+        'https://placekitten.com/200/287',
+        'https://placekitten.com/96/140',
+        'https://placekitten.com/96/139',
+        'https://placekitten.com/200/287',
+        'https://placekitten.com/96/140',
+        'https://placekitten.com/96/139',
+        'https://placekitten.com/200/287',
+    ];
+
+    useEffect(() => {
+        setMyArt(samples);
+    }, []);
 
     return (
         <main className={styles.home}>
-            <Header />
-            {myArt ? (
-                <div className={styles.homeArtlist}>
-                    <ArtList drawings={myArt} />
-                    <Button href="canvas" variant="contained">
-                        <AddIcon />
+            <img
+                src="./logo-horizontal.png"
+                alt="Fingerpaint"
+                className={styles.homeLogo}
+            />
+            <section></section>
+            <AppBar
+                position="absolute"
+                color="transparent"
+                sx={{ top: 'auto', bottom: 0, boxShadow: 'none' }}
+            >
+                <Toolbar
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <div
+                        style={{
+                            margin: '0 3rem',
+                        }}
+                    />
+                    <Button
+                        variant="text"
+                        style={{
+                            margin: '0 1rem',
+                        }}
+                    >
+                        <img src="./new.png" alt="NEW" />
                     </Button>
-                </div>
-            ) : (
-                <div className={styles.homeNoArtlist}>
-                    <Welcome />
-                    <Button href="canvas" variant="contained" size="large">
-                        <AddIcon /> Create New Art
+                    <Button
+                        variant="text"
+                        style={{
+                            margin: '1rem',
+                        }}
+                    >
+                        <img src="./trash.png" alt="DELETE" />
                     </Button>
-                </div>
-            )}
+                </Toolbar>
+            </AppBar>
         </main>
     );
 }
