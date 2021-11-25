91bf56450f317691f6a3a891bf4a6784ee5fad79
Julianne Vela <75726146+juliannevela@users.noreply.github.com>
2021-11-25 01:18:48 -0800

    moved files into src from public to reduce build size and removed reference for public_url in sw.

--- src/service-worker.js
+++ src/service-worker.js
@@ -43,7 +43,7 @@ registerRoute(
 
     return true;
   },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
+  createHandlerBoundToURL(/src/ + '/index.html')
 );
 
 // An example runtime caching route for requests that aren't handled by the
@@ -51,13 +51,13 @@ registerRoute(
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
   ({ url }) =>
-    url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
+    url.origin === self.location.origin && url.pathname.endsWith('.svg'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
     cacheName: 'images',
     plugins: [
       // Ensure that once this runtime cache reaches a maximum size the
       // least-recently used images are removed.
-      new ExpirationPlugin({ maxEntries: 50 }),
+      new ExpirationPlugin({ maxEntries: 150 }),
     ],
   })
 );
