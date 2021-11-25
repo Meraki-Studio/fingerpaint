22ed0058c1141def82711031116c429410dbe111
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-11-25 10:05:17 -0800

    landing page content update | adjustment needed

--- src/containers/landing/Landing.jsx
+++ src/containers/landing/Landing.jsx
@@ -4,6 +4,35 @@ import { makers } from './data';
 import './Landing.scss';
 
 const logo = new URL('../../../public/assets/Logo_horiz.png', import.meta.url);
+const mockup = new URL(
+  '../../../public/assets/mainmockupOriginal.png',
+  import.meta.url
+);
+const painting = new URL(
+  '../../../public/assets/painting.png',
+  import.meta.url
+);
+const sticker = new URL('../../../public/assets/sticker.png', import.meta.url);
+const Tutorial1 = new URL(
+  '../../../public/assets/Tutorial-1.png',
+  import.meta.url
+);
+const Tutorial2 = new URL(
+  '../../../public/assets/Tutorial-2.png',
+  import.meta.url
+);
+const Tutorial3 = new URL(
+  '../../../public/assets/Tutorial-3.png',
+  import.meta.url
+);
+const watercolor = new URL(
+  '../../../public/assets/watercolor.png',
+  import.meta.url
+);
+const logoMain = new URL(
+  '../../../public/assets/LogoMain.png',
+  import.meta.url
+);
 
 const Landing = () => (
   <>
@@ -33,16 +62,12 @@ const Landing = () => (
             </button>
           </article>
           <figure className="heroImage">
-            <img src="http://place-puppy.com/300x300" alt="Hero" />
+            <img src={mockup} alt="Hero" />
           </figure>
         </div>
       </div>
       <section className="what">
-        <img
-          className="whatImg"
-          src="http://place-puppy.com/500x500"
-          alt="Hero"
-        />
+        <img className="whatImg" src={logoMain} alt="Hero" />
         <div className="whatText">
           <h3>
             Fingerpaint: making digital design tools accessible and fun for your
@@ -51,9 +76,9 @@ const Landing = () => (
           <p>
             At last, a mobile app that preliterate kids can enjoy right off the
             bat. Designed for children ages 2 - 5, Fingerpaint equips your child
-            with basic design tools that are intuitive to navigate. Fingerpaint
-            is a Progressive Web App built using the React Framework and
-            Material UI for the front-end components. We also utilized
+            with basic design tools that are intuitive to navigate. <br />
+            Fingerpaint is a Progressive Web App built using the React Framework
+            and Material UI for the front-end components. We also utilized
             React-Canvas-Draw and React Color in order to make the canvas more
             accessible and appealing to young children.
           </p>
@@ -62,7 +87,7 @@ const Landing = () => (
       <section className="features">
         <article className="featureCard">
           <figure>
-            <img src="http://place-puppy.com/50x50" alt="feature" />
+            <img src={painting} alt="feature" />
             <figcaption>Interactive Canvas</figcaption>
           </figure>
           <p>
@@ -73,7 +98,7 @@ const Landing = () => (
         </article>
         <article className="featureCard">
           <figure>
-            <img src="http://place-puppy.com/50x50" alt="feature" />
+            <img src={sticker} alt="feature" />
             <figcaption>Fun stickers</figcaption>
           </figure>
           <p>
@@ -84,7 +109,7 @@ const Landing = () => (
         </article>
         <article className="featureCard">
           <figure>
-            <img src="http://place-puppy.com/50x50" alt="feature" />
+            <img src={watercolor} alt="feature" />
             <figcaption>Minimal interface</figcaption>
           </figure>
           <p>
@@ -102,30 +127,23 @@ const Landing = () => (
           <div className="howText">
             <h3>Step 1: Select a tool to use</h3>
             <p>
-              Choose from a variety of brushes, a rainbow of hues, a collection
-              of stamps, and a panorama of backgrounds. Situated in the tools
-              dock below, it’s as simple as tapping the one you want.
+              Choose from a paintbrush, a stamp, a backdrop editor, and or an
+              eraser. Situated in the tools dock below, it’s as simple as
+              tapping the one you want.
             </p>
           </div>
-          <img
-            className="howImg"
-            src="http://place-puppy.com/500x500"
-            alt="Hero"
-          />
+          <img className="howImg" src={Tutorial1} alt="Select a tool" />
         </div>
         <div className="how">
-          <img
-            className="howImg"
-            src="http://place-puppy.com/500x500"
-            alt="Hero"
-          />
+          <img className="howImg" src={Tutorial2} alt="Hero" />
           <div className="howText">
-            <h3>Step 2: Choose a style</h3>
+            <h3>Step 2: Choose a color or stamp and begin creating</h3>
             <p>
-              Upon selecting your tool of choice, you can now either choose your
-              favorite variation, or swipe the sub-menu down if you’re content
-              with what you already equipped. You can always return to the menu
-              by clicking on the tool icon again.
+              Upon selecting your tool of choice, you can now either choose from
+              a rainbow of colors, or choose from a collection of cute stamps.{' '}
+              <br />
+              You can reach this menu by tapping on the palette icon or selected
+              stamp located on the left side of the bottom controls.
             </p>
           </div>
         </div>
@@ -134,17 +152,14 @@ const Landing = () => (
             <h3>Step 3: Inspect, undo, hide</h3>
             <p>
               Enable view-mode with a single tap. Pinch and drag to zoom and
-              pan, without drawing. Tap the icon again to return to draw mode.
-              Made an accident? Tap the undo or redo icons accordingly. Want to
-              hide the interface? You can do that too! Simply tap the eye icon
-              to hide the interface. Tap again to unhide it.
+              pan, without drawing. Tap the icon again to return to draw mode.{' '}
+              <br />
+              Made an accident? Tap the undo or redo icons accordingly. <br />
+              Want to hide the interface? You can do that too! Simply tap the
+              eye icon to hide the interface. Tap again to unhide it.
             </p>
           </div>
-          <img
-            className="howImg"
-            src="http://place-puppy.com/500x500"
-            alt="Hero"
-          />
+          <img className="howImg" src={Tutorial3} alt="Hero" />
         </div>
       </section>
       <section className="team">
