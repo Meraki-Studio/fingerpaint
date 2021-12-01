f63b245f47e1bb0b11906b81d780411d296edc59
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-11-30 23:54:22 -0800

    bottom bar: palette and brush response palette and brush icons change color with color selected for drawing brush changes to basic black icon when erase is selected

--- src/containers/canvas/assets/Palette.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Palette() {
-  return <div></div>;
-}
---
+++ src/containers/canvas/buttons/Eraser.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+
+export default function Eraser() {
+  return (
+    <svg
+      width="61"
+      height="61"
+      viewBox="0 0 61 61"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="61" height="61" fill="#E5E5E5" />
+      <g id="Color picker - return to artboard" clip-path="url(#clip0_130_410)">
+        <rect
+          width="360"
+          height="640"
+          transform="translate(-283.043 -563.772)"
+          fill="white"
+        />
+        <g id="Bottom Control Bar" filter="url(#filter0_d_130_410)">
+          <rect
+            width="360"
+            height="92"
+            transform="translate(-283.043 -15.772)"
+            fill="white"
+          />
+          <g id="Button">
+            <g id="Icon" clip-path="url(#clip1_130_410)">
+              <path
+                id="Union"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M34.3849 19.6086L42.3399 27.5634C43.1624 28.3859 43.1624 29.7242 42.3399 30.5465L34.5506 38.3358L23.6123 27.3977L31.4017 19.6086C32.2241 18.7861 33.5624 18.7861 34.3849 19.6086ZM21.7893 29.2206C20.9483 30.0619 20.485 31.1802 20.485 32.3696C20.485 33.559 20.9483 34.6774 21.7893 35.5184L26.4298 40.1589C27.2708 40.9999 28.3893 41.4631 29.5787 41.4631C30.7683 41.4631 31.8864 40.9999 32.7277 40.1589L33.5563 39.3301L22.6181 28.3921L21.7893 29.2206ZM24.3402 40.0577H19.6596C19.2713 40.0577 18.9564 40.3727 18.9564 40.7609C18.9564 41.1493 19.2713 41.4641 19.6596 41.4641H25.7714C25.6562 41.3652 25.5439 41.2617 25.4356 41.1531L24.3402 40.0577Z"
+                fill="#393939"
+              />
+            </g>
+          </g>
+        </g>
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_130_410"
+          x="-313.043"
+          y="-47.772"
+          width="420"
+          height="152"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="-2" />
+          <feGaussianBlur stdDeviation="15" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_130_410"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_130_410"
+            result="shape"
+          />
+        </filter>
+        <clipPath id="clip0_130_410">
+          <rect
+            width="360"
+            height="640"
+            fill="white"
+            transform="translate(-283.043 -563.772)"
+          />
+        </clipPath>
+        <clipPath id="clip1_130_410">
+          <rect
+            width="24.0005"
+            height="24"
+            fill="white"
+            transform="translate(18.9564 18.228)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
